7fc8ddc268cd57caff4822bc2304ca5947a6496b
Martin Disch <martindisch@gmail.com>
2022-07-24 13:35:22 +0200

    Implement simple log matching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,85 @@
+use regex::Regex;
+
 fn main() {
     println!("Hello, world!");
+}
+
+/// The connection state change and affected player name.
+#[derive(Debug, PartialEq)]
+enum PlayerEvent {
+    Connected(String),
+    Disconnected(String),
+}
+
+impl PlayerEvent {
+    fn from_log(line: &str) -> Option<Self> {
+        let event_regex =
+            Regex::new(r"Player (.+) (connected|disconnected)(.+)?\.")
+                .unwrap();
+
+        if let Some(captures) = event_regex.captures(line) {
+            if &captures[2] == "connected" {
+                return Some(Self::Connected(captures[1].to_string()));
+            } else {
+                return Some(Self::Disconnected(captures[1].to_string()));
+            }
+        }
+
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn normal_name() {
+        let event = PlayerEvent::from_log(
+            "12:46:02 Player johndoe connected (id=12345678987654321).",
+        );
+
+        assert_eq!(Some(PlayerEvent::Connected("johndoe".to_string())), event);
+    }
+
+    #[test]
+    fn long_name() {
+        let event = PlayerEvent::from_log(
+            "12:46:02 Player John Doe connected (id=12345678987654321).",
+        );
+
+        assert_eq!(
+            Some(PlayerEvent::Connected("John Doe".to_string())),
+            event
+        );
+    }
+
+    #[test]
+    fn disconnect() {
+        let event =
+            PlayerEvent::from_log("12:46:02 Player johndoe disconnected.");
+
+        assert_eq!(
+            Some(PlayerEvent::Disconnected("johndoe".to_string())),
+            event
+        );
+    }
+
+    #[test]
+    fn battleye() {
+        let event = PlayerEvent::from_log(
+            "12:46:21 BattlEye Server: Player #0 johndoe disconnected",
+        );
+
+        assert_eq!(None, event);
+    }
+
+    #[test]
+    fn other() {
+        let event = PlayerEvent::from_log(
+            "12:46:31 All users disconnected, waiting for users.",
+        );
+
+        assert_eq!(None, event);
+    }
 }
